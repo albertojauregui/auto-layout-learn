00b324137f2313b447f3c37c69b2a55239065d7e
albertojauregui <bastertwo@gmail.com>
2017-10-16 22:36:46 -0600

    Initial Commit

---
+++ AutoLayoutLearn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutLearn
+//
+//  Created by Alberto Jauregui on 16/10/17.
+//  Copyright © 2017 Alberto Jauregui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
